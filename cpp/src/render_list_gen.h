9b325679b06186c13c2949ce6e793294e238912a
xe640 <xe640@inbox.lv>
2026-09-15 14:15:19 +0300

    render list data structures

---
+++ cpp/src/render_list_gen.h
@@ -0,0 +1,51 @@
+#include "../include/GLM/glm.hpp"
+#include "../include/GLM/gtc/matrix_transform.hpp"
+#include "../include/GLM/gtc/type_ptr.hpp"
+
+#define MAX_LIST_ELEMENTS 4096
+
+struct plane {
+    glm::vec3 position;
+    float size;
+    glm::vec3 normal;
+    glm::vec4 colour;
+};
+
+struct line {
+    glm::vec3 position1;
+    float size1;
+    glm::vec4 colour1;
+    glm::vec3 position2;
+    float size2;
+    glm::vec4 colour2;
+};
+
+struct plane_list {
+    glm::vec4 values[MAX_LIST_ELEMENTS * 3];
+    int numPlanes;
+};
+
+struct line_list {
+    glm::vec4 values[MAX_LIST_ELEMENTS * 4];
+    int numLines;
+};
+
+struct render_list {
+    plane_list* planeList;
+    line_list* lineList;
+};
+
+struct AABB {
+    glm::vec3 min;
+    glm::vec3 max;
+};
+
+class RenderListGen {
+public:
+    static void clearLists(render_list* list);
+    static void renderPlane(render_list* list, plane toAdd);
+    static void renderLine(render_list* list, line toAdd);
+    static void renderPlaneTowards(render_list* list, plane toAdd, glm::vec3 target_pos);
+    static void renderPlaneWire(render_list* list, plane toAdd, float lineThickness);
+    static void renderAABB(render_list* list, AABB toAdd, glm::vec4 colour, float lineThickness);
+};
